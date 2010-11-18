8a9808dd100f377f81c4893bc30f03912ca28eee
Jeff Tougas <tougasj@gmail.com>
2010-11-17 23:16:23 -0500

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,6 @@
+# TextCare Twilio SMS application
+
+This is a Twilio application built in Ruby on Rails for medical providers to retrieve and record simple information about patients.
+
+by:
+[Jeff Tougas](http://jefftougas.com/), Esther Estroff, Corey Steward, Stephen Hilber
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Textcare::Application.config.secret_token = 'e3cf608d81680214fe0781d8317eaea70a402bdc90d6f96f8b6e4135e5ab8a49322c478181fc6794484765ca781f6665b5e70560868a64d2614d9082416c59db'
